625d55cb27814e4eb2affd4074d62d92cffcd4c3
Edgar Fernandez <edgpau@gmail.com>
2025-03-11 23:08:14 -0400

    Shopping

---
+++ main.js
@@ -0,0 +1,10 @@
+
+//loop throu print one by 1
+const array=[1,2,3,4,5]
+console.log(typeof([1,2,3,4,5]))
+
+ for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element)
+
+}
