3021b32359d78f49b673fe76b04d518534ac0b0a
Edgar Fernandez <edgpau@gmail.com>
2025-03-18 21:08:05 -0400

    Questions 6

--- main.js
+++ main.js
@@ -1,19 +1,72 @@
 
-for (let number = 0; number <= 30; number++) {
-    if (number % 5 === 0 && number % 3 === 0) {
-        console.log(" FizzBuzz.");
-    } else {
-        if (number % 5 === 0) {
-            console.log(" Buzz.");
-        } else {
-            if (number % 3 === 0) {
-                console.log(" Fizz.");
-            } else {
-                console.log(number);
-            }
-        }
+// Reversing a string
+
+
+function reverseString(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
     }
-
-
+    return reversed;
 }
 
+console.log(reverseString("water"));
+
+// finding min & max
+
+let numbers = [10, 5, 20, 8, 15];
+
+let min = Math.min(...numbers);
+let max = Math.max(...numbers);
+
+console.log("Min:", min); 
+console.log("Max:", max);
+
+
+// Finding and removing duplicates
+
+let numbers1 = [1, 2, 3, 2, 4, 5, 1, 6];
+
+let uniqueNumbers = [...new Set(numbers1)];
+
+console.log(uniqueNumbers);
+
+// Check if a string is a pallindrome
+
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+console.log(isPalindrome("12321"));
+
+
+// Fibonacci series
+
+function fibonacciSeries(n) {
+    let fib = [0, 1]; // Starting values
+
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib;
+}
+
+console.log(fibonacciSeries(10)); 
+
+// find factorial
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+console.log(factorial(5));
+
+//  Primes (is prime print few)
+
+// find missing number from 1 to N
--- main2.js
+++ main2.js
@@ -44,3 +44,44 @@ for (let number = 0; number <= 30; number++) {
 
 }
 
+
+// Reversing a string
+
+
+function reverseString(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+console.log(reverseString("water"));
+
+// finding min & max
+
+let numbers = [10, 5, 20, 8, 15];
+
+let min = Math.min(...numbers);
+let max = Math.max(...numbers);
+
+console.log("Min:", min); 
+console.log("Max:", max);
+
+
+// Finding and removing duplicates
+
+let numbers1 = [1, 2, 3, 2, 4, 5, 1, 6];
+
+let uniqueNumbers = [...new Set(numbers1)];
+
+console.log(uniqueNumbers);
+
+// Check if a string is a pallindrome
+
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+console.log(isPalindrome("12321"));
